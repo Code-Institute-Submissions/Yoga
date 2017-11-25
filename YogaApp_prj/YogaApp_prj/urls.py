8bba2445b12f53b9bf8b1772866fde0b366e48b7
Kate Billington <katehbillington@gmail.com>
2017-11-25 11:20:04 +0000

    New commit, updated files

--- YogaApp_prj/YogaApp_prj/urls.py
+++ YogaApp_prj/YogaApp_prj/urls.py
@@ -2,18 +2,20 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from Yoga_app import views as Yoga_app_views
 from accounts import views as accounts_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', Yoga_app_views.say_yoga),
-    url(r'^now$', Yoga_app_views.get_now),
+    url(r'^admin/', admin.site.urls),
     url(r'^timetable$', Yoga_app_views.timetable_page),
-    url(r'^contacts/', Yoga_app_views.get_contacts),
-    url(r'^poseblog/$', Yoga_app_views.post_list),
+    url(r'^blog/', Yoga_app_views.post_list),
     url(r'^poseblog/(?P<id>\d+)/$', Yoga_app_views.post_detail),
     url(r'^poseblog/new/$', Yoga_app_views.new_post, name='new_post'),
     url(r'^poseblog/(?P<id>\d+)/edit$', Yoga_app_views.edit_post, name='edit'),
     url(r'^register/$', accounts_views.register, name='register'),
     url(r'^profile/$', accounts_views.profile, name='profile'),
+    url(r'^login/$', accounts_views.login, name='login'),
+    url(r'^logout/$', accounts_views.logout, name='logout'),
+    url(r'^cancel_subscription/$', accounts_views.cancel_subscription, name='cancel_subscription'),
 ]
