5cb99fe3e2cf8a9d4032985e3183ee58ac2b66dc
Kate Billington <katehbillington@gmail.com>
2017-12-13 18:44:12 +0000

    Added staging

--- YogaApp_prj/settings/staging.py
+++ YogaApp_prj/settings/staging.py
@@ -1,12 +1,12 @@
 from settings.base import *
 
+import dj_database_url
+
+
 DEBUG = False
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 
